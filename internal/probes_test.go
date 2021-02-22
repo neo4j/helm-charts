22fd8f7a6e3b6b03f88241b19bf775bebc89591f
Bledi Feshti <bledifeshti@neotechnology.com>
2021-02-22 14:19:37 +0100

    Added Liveness/Readiness probes regression test

---
+++ internal/probes_test.go
@@ -0,0 +1,82 @@
+package internal
+
+import (
+	"context"
+	"fmt"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+	"k8s.io/client-go/tools/clientcmd"
+	"path"
+	"runtime"
+)
+
+func CheckProbes() error {
+	// uses the current context in kubeconfig
+	// path-to-kubeconfig -- for example, /root/.kube/config
+	_, filename, _, _ := runtime.Caller(0)
+	currentDir := path.Dir(filename)
+	dir := path.Join(currentDir, "..")
+	config, err := clientcmd.BuildConfigFromFlags("", path.Join(dir, ".kube/config"))
+	if err != nil {
+		return fmt.Errorf("Unable to read kubeconfig file: %v", err)
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return fmt.Errorf("Unable to create kubernetes client: %v", err)
+	}
+	pods, err := clientset.CoreV1().Pods("neo4j").List(context.TODO(), v1.ListOptions{})
+	if err != nil {
+		return fmt.Errorf("Failed to get Pods options: %v", err)
+	}
+
+	// getting Probes values from values.yaml
+	type ValuesYaml struct {
+		ReadinessProbe struct {
+			FailureThreshold int32 `yaml:"failureThreshold"`
+			TimeoutSeconds   int32 `yaml:"timeoutSeconds"`
+			PeriodSeconds    int32 `yaml:"periodSeconds"`
+		} `yaml:"readinessProbe"`
+		LivenessProbe struct {
+			FailureThreshold int32 `yaml:"failureThreshold"`
+			TimeoutSeconds   int32 `yaml:"timeoutSeconds"`
+			PeriodSeconds    int32 `yaml:"periodSeconds"`
+		} `yaml:"livenessProbe"`
+	}
+
+	var fileName string = "neo4j/values.yaml"
+
+	yamlFile, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		return fmt.Errorf("Error reading YAML file: %v", err)
+	}
+
+	var yamlConfig ValuesYaml
+	err = yaml.Unmarshal(yamlFile, &yamlConfig)
+	if err != nil {
+		return fmt.Errorf("Error parsing YAML file: %v", err)
+	}
+	pods_map := make(map[string]int32)
+	for _, opt := range pods.Items {
+		for _, container := range opt.Spec.Containers {
+			pods_map[container.Name + "_LivenessProbe"] = container.LivenessProbe.PeriodSeconds
+			pods_map[container.Name + "_ReadinessProbe"] = container.ReadinessProbe.PeriodSeconds
+		}
+	}
+	podsLiveness := "neo4j_LivenessProbe"
+	yamlConfigLiveness := yamlConfig.LivenessProbe.PeriodSeconds
+	if pods_map[podsLiveness] != yamlConfigLiveness {
+
+		return fmt.Errorf("LivenessProbe mismatch.Expected: %v but got %v", yamlConfigLiveness, podsLiveness )
+	}
+	podsReadiness := "neo4j_ReadinessProbe"
+	yamlConfigReadiness := yamlConfig.ReadinessProbe.PeriodSeconds
+	if pods_map[podsReadiness] != yamlConfigReadiness {
+
+		return fmt.Errorf("ReadinessProbe mismatch.Expected: %v but got %v", yamlConfigReadiness, podsReadiness )
+	}
+	return nil
+}
