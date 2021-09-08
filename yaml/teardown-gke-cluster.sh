afba65cc86b942c03cfc76215638a08935aea56d
Andrew Jefferson <8148776+eastlondoner@users.noreply.github.com>
2021-09-08 09:50:34 +0200

    remove static yaml (#69)

--- yaml/teardown-gke-cluster.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-CLUSTER_NAME="sandbox"
-PROJECT_ID="jenny-288814"
-ZONE="europe-west1-b"
-NODE_MACHINE="n1-standard-1"
-
-gcloud container clusters delete "${CLUSTER_NAME}" --zone "${ZONE}" --project "${PROJECT_ID}"
