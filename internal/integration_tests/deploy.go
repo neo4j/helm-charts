d96c5fc50f369c7572cc3afc5124ba15dc2e1836
Harshit Singhvi <harshit.singhvi@neotechnology.com>
2022-07-19 21:49:46 +0100

    Refactored integration and unit test cases

--- internal/integration_tests/deploy.go
+++ internal/integration_tests/deploy.go
@@ -14,6 +14,7 @@ import (
 	. "github.com/neo4j/helm-charts/internal/helpers"
 	"github.com/neo4j/helm-charts/internal/integration_tests/gcloud"
 	"github.com/neo4j/helm-charts/internal/model"
+	"github.com/stretchr/testify/assert"
 	"io"
 	"k8s.io/client-go/kubernetes"
 	restclient "k8s.io/client-go/rest"
@@ -28,6 +29,11 @@ import (
 	"testing"
 	"time"
 )
+
+type SubTest struct {
+	name string
+	test func(*testing.T)
+}
 
 func CheckError(err error) {
 	if err != nil {
@@ -362,4 +368,68 @@ func prepareK8s(t *testing.T, releaseName model.ReleaseName) (Closeable, error) 
 	}
 
 	return AsCloseable(closeables), nil
+}
+
+func runSubTests(t *testing.T, subTests []SubTest) {
+	t.Cleanup(func() { t.Logf("Finished running all tests in '%s'", t.Name()) })
+
+	for _, test := range subTests {
+
+		t.Run(test.name, func(t *testing.T) {
+			t.Logf("Started running subtest '%s'", t.Name())
+			t.Cleanup(func() { t.Logf("Finished running subtest '%s'", t.Name()) })
+			test.test(t)
+		})
+	}
+}
+
+func installNeo4j(t *testing.T, releaseName model.ReleaseName, chart model.Neo4jHelmChart, extraHelmInstallArgs ...string) (Closeable, error) {
+	closeables := []Closeable{}
+	addCloseable := func(closeable Closeable) {
+		closeables = append([]Closeable{closeable}, closeables...)
+	}
+
+	closeable, err := prepareK8s(t, releaseName)
+	addCloseable(closeable)
+	if err != nil {
+		return AsCloseable(closeables), err
+	}
+
+	closeable, err = InstallNeo4jInGcloud(t, gcloud.CurrentZone(), gcloud.CurrentProject(), releaseName, chart, extraHelmInstallArgs...)
+	addCloseable(closeable)
+	if err != nil {
+		return AsCloseable(closeables), err
+	}
+
+	err = run(t, "kubectl", "--namespace", string(releaseName.Namespace()), "rollout", "status", "--watch", "--timeout=120s", "statefulset/"+releaseName.String())
+	return AsCloseable(closeables), err
+}
+
+func k8sTests(name model.ReleaseName, chart model.Neo4jHelmChart) ([]SubTest, error) {
+	expectedConfiguration, err := (&model.Neo4jConfiguration{}).PopulateFromFile(Neo4jConfFile)
+	if err != nil {
+		return nil, err
+	}
+
+	return []SubTest{
+		{name: "Check Neo4j Logs For Any Errors", test: func(t *testing.T) {
+			t.Parallel()
+			assert.NoError(t, checkNeo4jLogsForAnyErrors(t, name), "Neo4j Logs check should succeed")
+		}},
+		{name: "Check Neo4j Configuration", test: func(t *testing.T) {
+			assert.NoError(t, checkNeo4jConfiguration(t, name, expectedConfiguration), "Neo4j Config check should succeed")
+		}},
+		{name: "Create Node", test: func(t *testing.T) { assert.NoError(t, createNode(t, name), "Create Node should succeed") }},
+		{name: "Delete Resources", test: func(t *testing.T) { assert.NoError(t, ResourcesCleanup(t, name), "Cleanup Resources should succeed") }},
+		{name: "Reinstall Resources", test: func(t *testing.T) {
+			assert.NoError(t, ResourcesReinstall(t, name, chart), "Reinstall Resources should succeed")
+		}},
+		{name: "Count Nodes", test: func(t *testing.T) { assert.NoError(t, checkNodeCount(t, name), "Count Nodes should succeed") }},
+		{name: "Check Probes", test: func(t *testing.T) { assert.NoError(t, CheckProbes(t, name), "Probes Matching should succeed") }},
+		{name: "Check Service Annotations", test: func(t *testing.T) {
+			assert.NoError(t, CheckServiceAnnotations(t, name, chart), "Services should have annotations")
+		}},
+		{name: "Check RunAsNonRoot", test: func(t *testing.T) { assert.NoError(t, RunAsNonRoot(t, name), "RunAsNonRoot check should succeed") }},
+		{name: "Exec in Pod", test: func(t *testing.T) { assert.NoError(t, CheckExecInPod(t, name), "Exec in Pod should succeed") }},
+	}, err
 }
--- internal/resources/resources.go
+++ internal/resources/resources.go
@@ -1,89 +1,27 @@
 package resources
 
-import (
-	"fmt"
-	"gopkg.in/yaml.v3"
-	"io/ioutil"
-	"os"
-	"path"
-	"path/filepath"
-	"runtime"
-)
-
-var _, thisFile, _, _ = runtime.Caller(0)
-var resourcesDir = path.Dir(thisFile)
-var TestAntiAffinityRule = newYamlFile("testAntiAffinityRule.yaml")
-var PluginsInitContainer = newYamlFile("pluginsInitContainer.yaml")
-var AcceptLicenseAgreementBoolYes = newYamlFile("acceptLicenseAgreementBoolYes.yaml")
-var AcceptLicenseAgreementBoolTrue = newYamlFile("acceptLicenseAgreementBoolTrue.yaml")
-var AcceptLicenseAgreement = newYamlFile("acceptLicenseAgreement.yaml")
-var ApocCorePlugin = newYamlFile("apocCorePlugin.yaml")
-var CsvMetrics = newYamlFile("csvMetrics.yaml")
-var DefaultStorageClass = newYamlFile("defaultStorageClass.yaml")
-var JvmAdditionalSettings = newYamlFile("jvmAdditionalSettings.yaml")
-var BoolsInConfig = newYamlFile("boolsInConfig.yaml")
-var IntsInConfig = newYamlFile("intsInConfig.yaml")
-var ChmodInitContainer = newYamlFile("chmodInitContainer.yaml")
-var ChmodInitContainerAndCustomInitContainer = newYamlFile("chmodInitContainerAndCustomInitContainer.yaml")
-var ReadReplicaUpstreamStrategy = newYamlFile("read_replica_upstream_selection_strategy.yaml")
-var ExcludeLoadBalancer = newYamlFile("excludeLoadBalancer.yaml")
-var EmptyImageCredentials = newYamlFile("imagePullSecret/emptyImageCreds.yaml")
-var DuplicateImageCredentials = newYamlFile("imagePullSecret/duplicateImageCreds.yaml")
-var MissingImageCredentials = newYamlFile("imagePullSecret/missingImageCreds.yaml")
-var EmptyImagePullSecrets = newYamlFile("imagePullSecret/emptyImagePullSecrets.yaml")
-var InvalidNodeSelectorLabels = newYamlFile("nodeselector.yaml")
-var ApocConfig = newYamlFile("apocConfig.yaml")
-var ApocClusterTestConfig = newYamlFile("apocClusterTest.yaml")
-var PodSpecAnnotations = newYamlFile("podSpecAnnotations.yaml")
-var StatefulSetAnnotations = newYamlFile("statefulSetAnnotations.yaml")
-var PriorityClassName = newYamlFile("priorityClassName.yaml")
-
-type YamlFile interface {
-	Path() string
-	HelmArgs() []string
-	Data() (map[interface{}]interface{}, error)
-}
-
-type yamlFile struct {
-	path string
-}
-
-func (y *yamlFile) Path() string {
-	return y.path
-}
-
-func (y *yamlFile) HelmArgs() []string {
-	return []string{"-f", y.path}
-}
-
-func (y *yamlFile) Data() (map[interface{}]interface{}, error) {
-	file, err := ioutil.ReadFile(y.Path())
-	if err != nil {
-		return nil, err
-	}
-	data := make(map[interface{}]interface{})
-	err = yaml.Unmarshal(file, &data)
-	if err != nil {
-		return nil, err
-	}
-	return data, nil
-}
-
-func resourceExistsAt(path string) (bool, error) {
-	if fileInfo, err := os.Stat(path); err == nil {
-		if filepath.Ext(path) == ".yaml" && !fileInfo.IsDir() {
-			return true, nil
-		}
-		return false, fmt.Errorf("unexpected error occured. File %s returned fileInfo: %v", path, fileInfo)
-	} else {
-		return false, err
-	}
-}
-
-func newYamlFile(filename string) YamlFile {
-	fullPath := path.Join(resourcesDir, filename)
-	if exists, err := resourceExistsAt(fullPath); err != nil || !exists {
-		panic(err)
-	}
-	return &yamlFile{fullPath}
-}
+var TestAntiAffinityRule = newYamlFile("yamls/testAntiAffinityRule.yaml")
+var PluginsInitContainer = newYamlFile("yamls/pluginsInitContainer.yaml")
+var AcceptLicenseAgreementBoolYes = newYamlFile("yamls/acceptLicenseAgreementBoolYes.yaml")
+var AcceptLicenseAgreementBoolTrue = newYamlFile("yamls/acceptLicenseAgreementBoolTrue.yaml")
+var AcceptLicenseAgreement = newYamlFile("yamls/acceptLicenseAgreement.yaml")
+var ApocCorePlugin = newYamlFile("yamls/apocCorePlugin.yaml")
+var CsvMetrics = newYamlFile("yamls/csvMetrics.yaml")
+var DefaultStorageClass = newYamlFile("yamls/defaultStorageClass.yaml")
+var JvmAdditionalSettings = newYamlFile("yamls/jvmAdditionalSettings.yaml")
+var BoolsInConfig = newYamlFile("yamls/boolsInConfig.yaml")
+var IntsInConfig = newYamlFile("yamls/intsInConfig.yaml")
+var ChmodInitContainer = newYamlFile("yamls/chmodInitContainer.yaml")
+var ChmodInitContainerAndCustomInitContainer = newYamlFile("yamls/chmodInitContainerAndCustomInitContainer.yaml")
+var ReadReplicaUpstreamStrategy = newYamlFile("yamls/read_replica_upstream_selection_strategy.yaml")
+var ExcludeLoadBalancer = newYamlFile("yamls/excludeLoadBalancer.yaml")
+var EmptyImageCredentials = newYamlFile("yamls/imagePullSecret/emptyImageCreds.yaml")
+var DuplicateImageCredentials = newYamlFile("yamls/imagePullSecret/duplicateImageCreds.yaml")
+var MissingImageCredentials = newYamlFile("yamls/imagePullSecret/missingImageCreds.yaml")
+var EmptyImagePullSecrets = newYamlFile("yamls/imagePullSecret/emptyImagePullSecrets.yaml")
+var InvalidNodeSelectorLabels = newYamlFile("yamls/nodeselector.yaml")
+var ApocConfig = newYamlFile("yamls/apocConfig.yaml")
+var ApocClusterTestConfig = newYamlFile("yamls/apocClusterTest.yaml")
+var PodSpecAnnotations = newYamlFile("yamls/podSpecAnnotations.yaml")
+var StatefulSetAnnotations = newYamlFile("yamls/statefulSetAnnotations.yaml")
+var PriorityClassName = newYamlFile("yamls/priorityClassName.yaml")
