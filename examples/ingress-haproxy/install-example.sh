b47671608ca928e48129e74418f3c706225505a5
Ollie Hughes <oliver.hughes@neotechnology.com>
2023-06-08 09:37:38 +0100

    fix lua

--- examples/ingress-haproxy/install-example.sh
+++ examples/ingress-haproxy/install-example.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 #readonly PROJECT_ROOT="$(dirname "$(dirname "$(dirname "$0")")")"
-readonly PROJECT_ROOT="$(builtin cd ../../; pwd)"
+readonly PROJECT_ROOT="$(dirname "$(dirname "$(dirname "$0")")")"
+
 readonly RELEASE_NAME=ingress-haproxy
 
 helm_install() {
@@ -19,11 +20,11 @@ helm_install() {
     done
     kubectl create secret generic test-auth --from-literal=NEO4J_AUTH=neo4j/password123
     kubectl apply -f  "${PROJECT_ROOT}/examples/ingress-haproxy/haproxy.yaml"
-#    kubectl get service -o jsonpath='{.status.loadBalancer.ingress[0].ip}'
+    kubectl get service -o jsonpath='{.status.loadBalancer.ingress[0].ip}'
     kubectl create ingress $RELEASE_NAME --class=nginx \
       --rule="${external_ip}.sslip.io/*=neo4j-haproxy:8080"
     for i in {1..3}; do
-        helm install "${RELEASE_NAME}-${i}" neo4j -f examples/ingress-haproxy/ingress-haproxy.yaml
+        helm upgrade --install "${RELEASE_NAME}-${i}" neo4j/neo4j -f examples/ingress-haproxy/ingress-haproxy.yaml
     done
 }
 
