0060a01e070603a3a4cc83bc6558f8f0de37af0b
Harshit Singhvi <89837129+harshitsinghvi22@users.noreply.github.com>
2023-09-20 10:56:46 +0100

    Fix/pod anti affinity (#231)
    * fixed bug with podAntiAffinity
    
    * added test case for testing podAntiAffinity
    
    * formatting go code
    
    ---------
    
    Co-authored-by: Harshit Singhvi <harshit.singhvi@neotechnology.com>

--- internal/unit_tests/helm_template_primaries_test.go
+++ internal/unit_tests/helm_template_primaries_test.go
@@ -817,6 +817,53 @@ func TestNeo4jPodNodeAffinity(t *testing.T) {
 	}))
 }
 
+// TestNeo4jPodAntiAffinity checks for podAntiAffinity setting in statefulset
+func TestNeo4jPodAntiAffinity(t *testing.T) {
+	t.Parallel()
+	helmValues := model.DefaultEnterpriseValues
+
+	forEachPrimaryChart(t, andEachSupportedEdition(func(t *testing.T, chart model.Neo4jHelmChartBuilder, edition string) {
+		if edition == "community" {
+			helmValues = model.DefaultCommunityValues
+		}
+		helmValues.PodSpec.PodAntiAffinity = model.PodAntiAffinity{
+			RequiredDuringSchedulingIgnoredDuringExecution: []model.RequiredDuringSchedulingIgnoredDuringExecution{
+				{
+					LabelSelector: model.LabelSelector{
+						MatchLabels: map[string]string{
+							"demo": "demo",
+						},
+					},
+					TopologyKey: "demo",
+				},
+			},
+		}
+		manifest, err := model.HelmTemplateFromStruct(t, model.HelmChart, helmValues)
+		assert.NoError(t, err)
+		neo4jStatefulSet := manifest.First(&appsv1.StatefulSet{}).(*appsv1.StatefulSet)
+		podAntiAffinity := neo4jStatefulSet.Spec.Template.Spec.Affinity.PodAntiAffinity
+		assert.NotNil(t, podAntiAffinity, "nil podAntiAffinity found")
+		assert.NotNil(t, podAntiAffinity.RequiredDuringSchedulingIgnoredDuringExecution)
+		assert.NotEqual(t, len(podAntiAffinity.RequiredDuringSchedulingIgnoredDuringExecution), 0)
+
+		helmValues.PodSpec.PodAntiAffinity = false
+		manifest, err = model.HelmTemplateFromStruct(t, model.HelmChart, helmValues)
+		assert.NoError(t, err)
+		neo4jStatefulSet = manifest.First(&appsv1.StatefulSet{}).(*appsv1.StatefulSet)
+		affinity := neo4jStatefulSet.Spec.Template.Spec.Affinity
+		assert.Nil(t, affinity, "affinity found")
+
+		helmValues.PodSpec.PodAntiAffinity = true
+		manifest, err = model.HelmTemplateFromStruct(t, model.HelmChart, helmValues)
+		assert.NoError(t, err)
+		neo4jStatefulSet = manifest.First(&appsv1.StatefulSet{}).(*appsv1.StatefulSet)
+		podAntiAffinity = neo4jStatefulSet.Spec.Template.Spec.Affinity.PodAntiAffinity
+		assert.NotNil(t, podAntiAffinity, "nil podAntiAffinity found")
+		assert.NotNil(t, podAntiAffinity.RequiredDuringSchedulingIgnoredDuringExecution)
+		assert.NotEqual(t, len(podAntiAffinity.RequiredDuringSchedulingIgnoredDuringExecution), 0)
+	}))
+}
+
 func TestExtraLabels(t *testing.T) {
 	t.Parallel()
 
