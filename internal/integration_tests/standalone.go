59d388fe0b9d9571da26c54086049998ab5ecbff
Harshit Singhvi <harshit.singhvi@neotechnology.com>
2024-02-13 14:51:01 +0000

    uncommenting the test cases

--- internal/integration_tests/standalone.go
+++ internal/integration_tests/standalone.go
@@ -615,22 +615,22 @@ func k8sTests(name model.ReleaseName, chart model.Neo4jHelmChartBuilder) ([]SubT
 		}},
 		{name: "Check RunAsNonRoot", test: func(t *testing.T) { assert.NoError(t, RunAsNonRoot(t, name), "RunAsNonRoot check should succeed") }},
 		{name: "Exec in Pod", test: func(t *testing.T) { assert.NoError(t, CheckExecInPod(t, name), "Exec in Pod should succeed") }},
-		//{name: "Install Backup Helm Chart For GCP With Workload Identity", test: func(t *testing.T) {
-		//	t.Parallel()
-		//	assert.NoError(t, InstallNeo4jBackupGCPHelmChartWithWorkloadIdentity(t, name), "Backup to GCP with workload identity should succeed")
-		//}},
-		//{name: "Install Backup Helm Chart For AWS", test: func(t *testing.T) {
-		//	t.Parallel()
-		//	assert.NoError(t, InstallNeo4jBackupAWSHelmChart(t, name), "Backup to AWS should succeed")
-		//}},
-		//{name: "Install Backup Helm Chart For Azure", test: func(t *testing.T) {
-		//	t.Parallel()
-		//	assert.NoError(t, InstallNeo4jBackupAzureHelmChart(t, name), "Backup to Azure should succeed")
-		//}},
-		//{name: "Install Backup Helm Chart For GCP", test: func(t *testing.T) {
-		//	t.Parallel()
-		//	assert.NoError(t, InstallNeo4jBackupGCPHelmChart(t, name), "Backup to GCP should succeed")
-		//}},
+		{name: "Install Backup Helm Chart For GCP With Workload Identity", test: func(t *testing.T) {
+			t.Parallel()
+			assert.NoError(t, InstallNeo4jBackupGCPHelmChartWithWorkloadIdentity(t, name), "Backup to GCP with workload identity should succeed")
+		}},
+		{name: "Install Backup Helm Chart For AWS", test: func(t *testing.T) {
+			t.Parallel()
+			assert.NoError(t, InstallNeo4jBackupAWSHelmChart(t, name), "Backup to AWS should succeed")
+		}},
+		{name: "Install Backup Helm Chart For Azure", test: func(t *testing.T) {
+			t.Parallel()
+			assert.NoError(t, InstallNeo4jBackupAzureHelmChart(t, name), "Backup to Azure should succeed")
+		}},
+		{name: "Install Backup Helm Chart For GCP", test: func(t *testing.T) {
+			t.Parallel()
+			assert.NoError(t, InstallNeo4jBackupGCPHelmChart(t, name), "Backup to GCP should succeed")
+		}},
 		{name: "Install Reverse Proxy Helm Chart", test: func(t *testing.T) {
 			t.Parallel()
 			assert.NoError(t, InstallReverseProxyHelmChart(t, name), "Reverse Proxy installation with ingress should succeed")
