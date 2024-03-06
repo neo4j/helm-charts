941445bc20484f4eab9e76189eac886b3a543f55
Harshit Singhvi <harshit.singhvi@neotechnology.com>
2024-03-06 16:14:30 +0000

    debugging issue

--- neo4j-admin/backup/neo4j-admin/operations.go
+++ neo4j-admin/backup/neo4j-admin/operations.go
@@ -52,7 +52,7 @@ func PerformConsistencyCheck(backupFileName string, database string) (string, er
 	var me *exec.ExitError
 	if errors.As(err, &me) {
 		log.Printf("Inconsistencies found for %s database. Exit code was %d\n", database, me.ExitCode())
-		log.Printf("Consistency Check Completed !!")
+		log.Printf("Consistency Check Completed !! \n Output := %v", string(output))
 
 		tarFileName := fmt.Sprintf("/backups/%s.report.tar.gz", backupFileName)
 		directoryName := fmt.Sprintf("/backups/%s.report", backupFileName)
