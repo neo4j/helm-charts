64fea0dad9491d58b4b2acd5214ffc9299f093ec
Bledi Feshti <bledifeshti@neotechnology.com>
2025-01-30 14:19:28 +0100

    fix devenv

--- internal/unit_tests/backup_operations_test.go
+++ internal/unit_tests/backup_operations_test.go
@@ -6,6 +6,8 @@ import (
 	"testing"
 
 	"github.com/neo4j/helm-charts/internal/backup"
+	"github.com/stretchr/testify/assert"
+)
 
 func TestDeleteBackupFiles(t *testing.T) {
 	t.Parallel()
@@ -55,5 +57,3 @@ func TestDeleteBackupFiles(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, len(testFiles), len(files), "Files should not be deleted when KEEP_BACKUP_FILES=true")
 }
-func TestGetAggregateBackupCommandFlags(t *testing.T) {
-	t.Parallel()
