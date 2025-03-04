a20e446788884859819c49bbfabad53af5a933e4
Bledi Feshti <bledifeshti@neotechnology.com>
2025-03-05 00:11:24 +0100

    add configuration parameters for s3 client

--- neo4j-admin/backup/aws/client.go
+++ neo4j-admin/backup/aws/client.go
@@ -3,9 +3,10 @@ package aws
 import (
 	"context"
 	"fmt"
+	"os"
+
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
-	"os"
 )
 
 type awsClient struct {
@@ -15,28 +16,44 @@ type awsClient struct {
 func NewAwsClient(credentialPath string) (*awsClient, error) {
 	var cfg aws.Config
 	var err error
+
+	// Check if a custom region is specified
+	customRegion := os.Getenv("S3_REGION")
+
 	if credentialPath == "/credentials/" {
 		_, present := os.LookupEnv("AWS_WEB_IDENTITY_TOKEN_FILE")
 		if !present {
 			return nil, fmt.Errorf("error while creating aws client without credentials file\n Missing AWS_WEB_IDENTITY_TOKEN_FILE")
 		}
+
+		// Use custom region if provided, otherwise use AWS_REGION
+		region := os.Getenv("AWS_REGION")
+		if customRegion != "" {
+			region = customRegion
+		}
+
 		cfg, err = config.LoadDefaultConfig(
 			context.TODO(),
-			config.WithRegion(os.Getenv("AWS_REGION")))
+			config.WithRegion(region))
 		if err != nil {
 			return nil, fmt.Errorf("error while creating aws client without credentials file\n %v", err)
 		}
 	} else {
+		// Load options for config
+		var options []func(*config.LoadOptions) error
 
-		cfg, err = config.LoadDefaultConfig(
-			context.TODO(),
-			config.WithSharedCredentialsFiles(
-				[]string{credentialPath},
-			))
+		// Add credentials file
+		options = append(options, config.WithSharedCredentialsFiles([]string{credentialPath}))
+
+		// Add custom region if provided
+		if customRegion != "" {
+			options = append(options, config.WithRegion(customRegion))
+		}
+
+		cfg, err = config.LoadDefaultConfig(context.TODO(), options...)
 		if err != nil {
 			return nil, fmt.Errorf("error while creating aws client \n %v", err)
 		}
-
 	}
 
 	return &awsClient{
