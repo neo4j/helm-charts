0816c846ee79cd44a07fb3d328ff29514c7ed7d1
Bledi Feshti <bledi.feshti@gmail.com>
2025-06-05 13:51:55 +0200

    Fix Azure blob storage url

--- internal/integration_tests/standalone.go
+++ internal/integration_tests/standalone.go
@@ -972,6 +972,7 @@ func InstallNeo4jBackupAzureHelmChart(t *testing.T, standaloneReleaseName model.
 		DatabaseNamespace:        string(standaloneReleaseName.Namespace()),
 		Database:                 "neo4j,system",
 		CloudProvider:            "azure",
+		AzureStorageAccountName:  "helmbackuptest", // Set Azure storage account name for proper URI format
 		SecretName:               "azurecred",
 		SecretKeyName:            "credentials",
 		Verbose:                  true,
