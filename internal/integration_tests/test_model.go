44616b82cf768fa1aa637ad7d2f70e139f74d0b4
Rigert Alekaj <rigert.alekaj@neo4j.com>
2025-06-30 16:38:46 +0200

    reduced random generated ID to 3 digits

--- internal/integration_tests/test_model.go
+++ internal/integration_tests/test_model.go
@@ -18,7 +18,7 @@ var Neo4jConfFile = fmt.Sprintf("neo4j/neo4j-%s.conf", model.Neo4jEdition)
 func init() {
 	dt := time.Now()
 	// Include microseconds and random component for uniqueness
-	dateTag := dt.Format("15:04:05.000000 Mon")
+	dateTag := dt.Format("15:04:05.0000 Mon")
 	// Add a small random component to ensure uniqueness even within microseconds
 	randomSuffix := rand.Intn(1000)
 	dateTag = fmt.Sprintf("%s-%d", dateTag, randomSuffix)
