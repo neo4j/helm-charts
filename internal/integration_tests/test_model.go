1103b197f2b73423ffb4d97c1c8311e40c7a3a74
Rigert Alekaj <rigert.alekaj@neo4j.com>
2025-06-30 16:01:28 +0200

    fixed multiple issues with randomly failing tests

--- internal/integration_tests/test_model.go
+++ internal/integration_tests/test_model.go
@@ -2,9 +2,11 @@ package integration_tests
 
 import (
 	"fmt"
-	"github.com/neo4j/helm-charts/internal/model"
+	"math/rand"
 	"strings"
 	"time"
+
+	"github.com/neo4j/helm-charts/internal/model"
 )
 
 var (
@@ -15,8 +17,13 @@ var Neo4jConfFile = fmt.Sprintf("neo4j/neo4j-%s.conf", model.Neo4jEdition)
 
 func init() {
 	dt := time.Now()
-	dateTag := dt.Format("15:04:05 Mon")
+	// Include microseconds and random component for uniqueness
+	dateTag := dt.Format("15:04:05.000000 Mon")
+	// Add a small random component to ensure uniqueness even within microseconds
+	randomSuffix := rand.Intn(1000)
+	dateTag = fmt.Sprintf("%s-%d", dateTag, randomSuffix)
 	dateTag = strings.ReplaceAll(dateTag, " ", "-")
 	dateTag = strings.ReplaceAll(dateTag, ":", "-")
+	dateTag = strings.ReplaceAll(dateTag, ".", "-")
 	TestRunIdentifier = strings.ToLower(dateTag)
 }
