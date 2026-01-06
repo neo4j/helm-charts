b807509cadf4268f7b1b81fbd5f51a9c3ab4a906
Rigert Alekaj <rigert.alekaj@neo4j.com>
2026-01-06 22:46:31 +0100

    add cleanup for lb on reinstall test

--- internal/integration_tests/persistent_data.go
+++ internal/integration_tests/persistent_data.go
@@ -1,6 +1,9 @@
 package integration_tests
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/neo4j/helm-charts/internal/model"
 	"testing"
 )
@@ -10,6 +13,19 @@ func ResourcesCleanup(t *testing.T, releaseName model.ReleaseName) error {
 }
 
 func ResourcesReinstall(t *testing.T, releaseName model.ReleaseName, chart model.Neo4jHelmChartBuilder) error {
+	namespace := string(releaseName.Namespace())
+	// Clean up the load balancer service and endpoint that might be left behind
+	// due to helm.sh/resource-policy: keep annotation when clustering is enabled
+	loadBalancerServiceName := fmt.Sprintf("%s-lb-neo4j", model.DefaultNeo4jName)
+	
+	// Delete the service if it exists (it might have resource-policy: keep)
+	_ = run(t, "kubectl", "delete", "service", loadBalancerServiceName, "--namespace", namespace, "--ignore-not-found=true")
+	
+	// Delete the endpoint if it exists (Kubernetes creates this automatically for services)
+	_ = run(t, "kubectl", "delete", "endpoints", loadBalancerServiceName, "--namespace", namespace, "--ignore-not-found=true")
+	
+	// Wait a moment for resources to be fully deleted
+	time.Sleep(2 * time.Second)
 
 	defaultHelmArgs := []string{}
 	defaultHelmArgs = append(defaultHelmArgs, model.DefaultNeo4jNameArg...)
@@ -20,7 +36,7 @@ func ResourcesReinstall(t *testing.T, releaseName model.ReleaseName, chart model
 		_ = run(t, "kubectl", "get", "events")
 		return err
 	}
-	err = run(t, "kubectl", "--namespace", string(releaseName.Namespace()), "rollout", "status", "--watch", "--timeout=120s", "statefulset/"+releaseName.String())
+	err = run(t, "kubectl", "--namespace", namespace, "rollout", "status", "--watch", "--timeout=120s", "statefulset/"+releaseName.String())
 	if err != nil {
 		t.Log("Helm Install failed:", err)
 		return err
